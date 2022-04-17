22e9a25b61f010c86d70f6c27a5194f438d1fbd4
natnael-OQP <makiya112545@gmail.com>
2022-04-17 16:45:21 +0300

    remove console.log

--- api/schema/resolvers/Product.js
+++ api/schema/resolvers/Product.js
@@ -2,7 +2,6 @@ const { db } = require('../../db')
 
 exports.Product = {
     category: (parent, args, context) => {
-        console.log(parent)
         return db.categories.find(
             (category) => category.id === parent.categoryId
         )
