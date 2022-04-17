54796d9bc527a40ec70f84e4d1724bf1b33b235b
natnael-OQP <makiya112545@gmail.com>
2022-04-17 18:06:29 +0300

    filtered onSale true products

--- api/schema/resolvers/Query.js
+++ api/schema/resolvers/Query.js
@@ -1,14 +1,14 @@
 exports.Query = {
-    products: (parent, args, { db }) => {
-        return db.products
-    },
-    product: (parent, { id }, { db }) => {
-        return db.products.find((product) => product.id === id)
-    },
-    categories: (parent, args, { db }) => {
-        return db.categories
-    },
-    category: (parent, { id }, { db }) => {
-        return db.categories.find((category) => category.id === id)
-    },
+    products: (parent, { filter }, { db }) =>
+        filter?.onSale
+            ? db.products.filter((product) => product.onSale === true)
+            : db.products,
+
+    product: (parent, { id }, { db }) =>
+        db.products.find((product) => product.id === id),
+
+    categories: (parent, args, { db }) => db.categories,
+
+    category: (parent, { id }, { db }) =>
+        db.categories.find((category) => category.id === id),
 }
