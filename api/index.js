bdddad4a8622dff2c2d8bb21db7c18e514e7127b
natnael-OQP <makiya112545@gmail.com>
2022-04-17 16:41:32 +0300

    import resolvers

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 const { ApolloServer } = require('apollo-server')
 const typeDefs = require('./schema/typeDefs')
-const resolvers = require('./schema/resolver')
+const resolvers = require('./schema/resolvers')
 
 const server = new ApolloServer({ typeDefs, resolvers })
 
