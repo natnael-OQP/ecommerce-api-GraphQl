28090509bdb833812842a2ffc0e8c94365a84869
natnael-OQP <makiya112545@gmail.com>
2022-04-17 16:44:05 +0300

    exports all

---
+++ api/schema/resolvers/index.js
@@ -0,0 +1,9 @@
+const { Product } = require('./Product')
+const { Query } = require('./Query')
+const { Category } = require('./Category')
+
+module.exports = {
+    Query,
+    Category,
+    Product,
+}
