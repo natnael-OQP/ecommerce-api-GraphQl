6131060ffe46e55addce05e9b28125646c02b752
natnael-OQP <makiya112545@gmail.com>
2022-04-17 22:45:32 +0300

    avgrating resolver

--- api/schema/resolvers/Query.js
+++ api/schema/resolvers/Query.js
@@ -1,9 +1,24 @@
 exports.Query = {
-    products: (parent, { filter }, { db }) =>
-        filter?.onSale
-            ? db.products.filter((product) => product.onSale === true)
-            : db.products,
-
+    products: (parent, { filter }, { db }) => {
+        let filteredProducts = db.products
+        if (filter) {
+            const { onSale, avgRating } = filter
+            if (onSale)
+                filteredProducts = filteredProducts.filter(
+                    (product) => product.onSale === true
+                )
+            if ([1, 2, 3, 4, 5].includes(avgRating))
+            filteredProducts = filteredProducts.filter((product) => {
+                    let sumRating = 0
+                    db.reviews.forEach((review) => {
+                        if ((review.productId = product.id))
+                            sumRating += review.rating
+                    })
+                    console.log(sumRating, product.name)
+                })
+        }
+        return filteredProducts
+    },
     product: (parent, { id }, { db }) =>
         db.products.find((product) => product.id === id),
 
