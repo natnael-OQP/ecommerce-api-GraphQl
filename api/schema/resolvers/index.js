74cb1d3c9a30ea66e98d35c6b5a10bdf6c74947a
natnael-OQP <makiya112545@gmail.com>
2022-04-18 18:06:05 +0300

    Mutation resolver

--- api/schema/resolvers/index.js
+++ api/schema/resolvers/index.js
@@ -1,9 +1,11 @@
 const { Product } = require('./Product')
 const { Query } = require('./Query')
 const { Category } = require('./Category')
+const { Mutation } = require('./Mutation')
 
 module.exports = {
     Query,
     Category,
     Product,
+    Mutation,
 }
