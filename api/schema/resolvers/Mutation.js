a0eb4f0d0a026f30516be28de478d0d227e2da21
natnael-OQP <makiya112545@gmail.com>
2022-04-18 21:24:07 +0300

    create category and generate random id

--- api/schema/resolvers/Mutation.js
+++ api/schema/resolvers/Mutation.js
@@ -1,9 +1,13 @@
+const { v4: uuid } = require('uuid')
+
 exports.Mutation = {
     addCategory: (parent, { input: { name } }, { db }) => {
-        db.categories.push({
+        const newCategory = {
             name,
-            id: Math.floor(Math.random(1) * 10),
-        })
-        return { name, id: Math.floor(Math.random(1) * 10) }
+            id: uuid(),
+        }
+
+        db.categories.push(newCategory)
+        return newCategory
     },
 }
